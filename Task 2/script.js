a62985f5ec367347d17b32fae529f14b7696d70d
Vinay Uday Meshram <146625204+iamvny@users.noreply.github.com>
2024-03-08 16:20:34 +0530

    Add files via upload

---
+++ Task 2/script.js
@@ -0,0 +1,17 @@
+function addTask() {
+  const taskInput = document.getElementById('taskInput');
+  const taskList = document.getElementById('taskList');
+
+  if (taskInput.value !== '') {
+      const li = document.createElement('li');
+      li.innerHTML = `${taskInput.value} <button onclick="removeTask(this)">Remove</button>`;
+      taskList.appendChild(li);
+      taskInput.value = '';
+  }
+}
+
+function removeTask(button) {
+  const li = button.parentNode;
+  const taskList = document.getElementById('taskList');
+  taskList.removeChild(li);
+}
